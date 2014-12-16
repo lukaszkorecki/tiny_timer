75e53d82df39203d53e5e134a940b3147899b149
Łukasz Korecki <lukasz@coffeesounds.com>
2014-12-16 11:23:40 +0000

    init

---
+++ tiny_timer.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'tiny_timer'
+  s.version     = '0.1.0'
+  s.platform    = Gem::Platform::RUBY
+  s.author      = 'Łukasz Korecki'
+  s.email       = 'lukasz@korecki.me'
+  s.summary     = 'tiny_timer!'
+  s.description = 'tiny_timer for simple measurements'
+
+  s.files         = ['tiny_timer.rb']
+  s.test_file     = 'tiny_timer_test.rb'
+  s.require_path  = '.'
+end
---
+++ tiny_timer.rb
@@ -0,0 +1,42 @@
+class TinyTimer
+  def initialize(name, dev = STDOUT, round = true)
+    @round = round
+    @dev = dev
+
+    @t = Time.now
+    @n = name
+    @timings = {}
+    pr 'Start'
+  end
+
+  def measure(name)
+    t_local = Time.now
+    r =  yield
+    t_e = Time.now
+
+    timings = { local: (t_e - t_local).to_f }
+
+    pr "#{name}: #{to_f timings[:local]}s"
+    @timings[name] = timings
+    r
+  end
+
+  def summarize
+    run_time = Time.now - @t
+    pr "Total run time: #{to_f run_time}s"
+    pr @timings.map { |k, t| "#{k}: #{p(run_time, t[:local])}" }.join(' | ')
+  end
+
+  def p(b, i)
+    per = (i.to_f * 100) / b.to_f
+    "#{to_f per}%"
+  end
+
+  def to_f(i)
+    @round ? i.to_f.round(2) : i
+  end
+
+  def pr(s)
+    @dev.write "|#{@n}| #{s}\n"
+  end
+end
